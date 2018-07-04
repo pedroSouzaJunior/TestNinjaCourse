3a59a07a4fd91c87b07f43868545a175d000fff0
Pedro <pedro.souza.psj@gmail.com>
2018-07-03 23:05:40 -0400

    Testando metodo que modifica o estado da aplicação

---
+++ UnitTesting/source-code-starter/TestNinja/TestNinja.UnitTests/ErrorLoggerTests.cs
@@ -0,0 +1,49 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class ErrorLoggerTests
+    {
+        private ErrorLogger logger;
+
+        [SetUp]
+        public void Setup()
+        {
+            logger = new ErrorLogger();
+        }
+        [Test]
+        public void Log_WhenCalled_SetThaLastErrorProperty()
+        {
+            logger.Log("a");
+
+            Assert.That(logger.LastError, Is.EqualTo("a"));
+        }
+
+        [Test]
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase(" ")]
+        public void Log_InvalidError_ThrowArgumentNullException(string error)
+        {
+            Assert.That(() => logger.Log(error), Throws.ArgumentNullException);
+        }
+
+        [Test]
+        public void Log_ValidError_RaiseErrorLoggedEvent()
+        {
+            var id = Guid.Empty;
+            logger.ErrorLogged += (sender, args) => { id = args; };
+
+            logger.Log("a");
+
+            Assert.That(id, Is.Not.EqualTo(Guid.Empty));
+        }
+    }
+}
