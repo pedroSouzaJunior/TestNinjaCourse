d4b25cf3d79dbdf5be4a49f8ba41bea16b9642d0
Pedro <pedro.souza.psj@gmail.com>
2018-07-03 23:05:09 -0400

    Testando metodo que retorna um valor tipado

---
+++ UnitTesting/source-code-starter/TestNinja/TestNinja.UnitTests/CustomerControllerTests.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class CustomerControllerTests
+    {
+        [Test]
+        public void GetCustomer_IdIsZero_ReturnNotFound()
+        {
+            var controller = new CustomerController();
+
+            var result = controller.GetCustomer(0);
+
+            Assert.That(result, Is.TypeOf<NotFound>());
+            //ou
+            Assert.That(result, Is.InstanceOf<NotFound>());
+        }
+
+        [Test]
+        public void GetCustomer_IdIsNotZero_ReturnOk()
+        {
+            var controller = new CustomerController();
+
+            var result = controller.GetCustomer(1);
+
+            Assert.That(result, Is.TypeOf<Ok>());
+            //ou
+            Assert.That(result, Is.InstanceOf<Ok>());
+        }
+    }
+}
