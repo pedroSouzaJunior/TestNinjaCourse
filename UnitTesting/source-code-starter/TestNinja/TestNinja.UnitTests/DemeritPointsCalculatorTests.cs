f8d950921f7acb4448f21db206ef29ea37a0d2d5
Pedro <pedro.souza.psj@gmail.com>
2018-07-11 23:37:26 -0400

    Iniciando Exercicios de teste unitario

---
+++ UnitTesting/source-code-starter/TestNinja/TestNinja.UnitTests/DemeritPointsCalculatorTests.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class DemeritPointsCalculatorTests
+    {
+        private DemeritPointsCalculator demerit;
+
+        [SetUp]
+        public void Setup()
+        {
+            demerit = new DemeritPointsCalculator();
+        }
+
+        [Test]
+        [TestCase(-1)]
+        [TestCase(301)]
+        public void CalculateDemeritPoints_WhenSpeedOutOfRange_ThrowException(int speed)
+        {
+            Assert.That(() => demerit.CalculateDemeritPoints(speed), Throws.TypeOf<ArgumentOutOfRangeException>());
+        }
+
+        [Test]
+        [TestCase(0, 0)]
+        [TestCase(64, 0)]
+        [TestCase(65, 0)]
+        [TestCase(66, 0)]
+        [TestCase(70, 1)]
+        [TestCase(75, 2)]
+        public void CalculateDemeritPoints_WhenSpeedLessEqualSpeedLimit_ReturnsZero(int speed, int expectedResult)
+        {
+            var result = demerit.CalculateDemeritPoints(speed);
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+    }
+}
---
+++ UnitTesting/source-code-starter/TestNinja/TestNinja.UnitTests/FizzBuzzTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class FizzBuzzTests
+    {
+        [Test]
+        [TestCase(3, "Fizz")]
+        [TestCase(5, "Buzz")]
+        [TestCase(15, "FizzBuzz")]
+        [TestCase(2, "2")]
+        public void GetOutput_WhenCalledWithNumberDivisibleForTree_ReturnFizz(int num, string expectedResult)
+        {
+            var result = FizzBuzz.GetOutput(num);
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+    }
+}
