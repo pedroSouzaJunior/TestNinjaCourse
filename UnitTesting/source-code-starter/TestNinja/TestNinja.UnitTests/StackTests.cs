238c59d54cf3ee431cb1c94f10b6b26796a735ea
Pedro <pedro.souza.psj@gmail.com>
2018-07-16 21:15:58 -0400

    - testes unitários para Stack.cs

--- UnitTesting/source-code-starter/TestNinja/TestNinja.UnitTests/StackTests.cs
+++ UnitTesting/source-code-starter/TestNinja/TestNinja.UnitTests/StackTests.cs
@@ -25,8 +25,72 @@ namespace TestNinja.UnitTests
         [Test]
         public void Push_WhenCalledWithNullObject_ThrowsArgumentNullException()
         {
-            Assert.That(()=>stack.Push((int?)null), Throws.ArgumentNullException);
-            
+            Assert.That(() => stack.Push((int?)null), Throws.ArgumentNullException);
+        }
+
+        [Test]
+        public void Push_EmptyStack_CountEqualsZero()
+        {
+            Assert.That(stack.Count, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void Pop_EmptyStack_ThrowsInvalidOperationException()
+        {
+            Assert.That(() => stack.Pop(), Throws.InvalidOperationException);
+        }
+
+        [Test]
+        public void Pop_StackWithFewObjects_ReturnObjectOnTheTop()
+        {
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+
+            var result = stack.Pop();
+
+            Assert.That(result, Is.EqualTo(3));
+        }
+
+        [Test]
+        public void Pop_StackWithFewObjects_DecrementsCount()
+        {
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Pop();
+
+            Assert.That(stack.Count, Is.EqualTo(2));
+        }
+
+
+        [Test]
+        public void Peek_EmptyStack_ThrowsInvalidOperationException()
+        {
+            Assert.That(() => stack.Peek(), Throws.InvalidOperationException);
+        }
+
+        [Test]
+        public void Peek_StackWithFewObjects_ReturnObjectOnTheTop()
+        {
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+
+            var result = stack.Peek();
+
+            Assert.That(result, Is.EqualTo(3));
+        }
+
+        [Test]
+        public void Peek_StackWithFewObjects_DoesNotRemoveTheObjectOnTheTop()
+        {
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Peek();
+
+            Assert.That(stack.Count, Is.EqualTo(3));
         }
     }
 }
